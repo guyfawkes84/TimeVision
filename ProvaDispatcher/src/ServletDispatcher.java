c4de7095fc366cdd3cc523f5fa5c45d60760add4
Nathan Ingram <valerio.carcaterra@gmail.com>
2019-07-23 15:28:44 +0200

    Add files via upload

---
+++ ProvaDispatcher/src/ServletDispatcher.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDispatcher
+ */
+@WebServlet("/ServletDispatcher")
+public class ServletDispatcher extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDispatcher() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("pagina.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		RequestDispatcher view;
+		
+		view = request.getRequestDispatcher("pagina.jsp");
+		
+		view.forward(request, response);
+	}
+
+}
